ab881692ad0c0699321717a253fd263b9ab1dbf1
mihir <mihirparmar031@gmail.com>
2023-04-05 11:11:55 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Sports sport = new Sports();
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("enter the game id:");
+    sport.setSportId(scanner.nextInt());
+    System.out.print("enter game name:");
+    sport.setSportName(scanner.next());
+    System.out.print("enter total players:");
+    sport.setTotalPlayer(scanner.nextInt());
+
+    System.out.println();
+    System.out.println("Sports Details:");
+    System.out.println("Sport id:"+sport.getSportId());
+    System.out.println("Sport Name:"+sport.getSportName());
+    System.out.println("total Player:"+sport.getTotalPlayer());
+  }
+
+
+}
+
---
+++ src/Sports.java
@@ -0,0 +1,30 @@
+class Sports{
+      private int sportId;
+      private String sportName;
+      private int totalPlayer;
+
+
+  public int getSportId() {
+    return sportId;
+  }
+
+  public void setSportId(int sportId) {
+    this.sportId = sportId;
+  }
+
+  public String getSportName() {
+    return sportName;
+  }
+
+  public void setSportName(String sportName) {
+    this.sportName = sportName;
+  }
+
+  public int getTotalPlayer() {
+    return totalPlayer;
+  }
+
+  public void setTotalPlayer(int totalPlayer) {
+    this.totalPlayer = totalPlayer;
+  }
+}
